ce2066507f9e3c419d8a96039d07b2ed1e0f74a8
Gagik Kazandjyan <bi0820a@outlook.com>
2023-07-10 17:49:29 +0400

    updated at 10/07/2023

--- src/main/java/com/epam/rd/autotasks/Words.java
+++ src/main/java/com/epam/rd/autotasks/Words.java
@@ -6,27 +6,21 @@ public class Words {
     public static String countWords(List<String> lines) {
         Map<String, Integer> wordCount = new HashMap<>();
 
-        // Iterate over each line of text
         for (String line : lines) {
             String[] words = line.split("[^\\p{L}\\p{Digit}]+"); // Split line into words
 
-            // Iterate over each word
             for (String word : words) {
-                // Ignore words with less than 4 characters
                 if (word.length() < 4) {
                     continue;
                 }
 
-                // Normalize the word to lowercase
                 String normalizedWord = word.toLowerCase();
 
-                // Count the occurrence of each word
                 int count = wordCount.getOrDefault(normalizedWord, 0);
                 wordCount.put(normalizedWord, count + 1);
             }
         }
 
-        // Filter out words that appear less than 10 times
         Iterator<Map.Entry<String, Integer>> iterator = wordCount.entrySet().iterator();
         while (iterator.hasNext()) {
             Map.Entry<String, Integer> entry = iterator.next();
@@ -35,7 +29,6 @@ public class Words {
             }
         }
 
-        // Sort the entries by amount and then alphabetically
         List<Map.Entry<String, Integer>> sortedEntries = new ArrayList<>(wordCount.entrySet());
         sortedEntries.sort(new Comparator<Map.Entry<String, Integer>>() {
             @Override
@@ -48,15 +41,11 @@ public class Words {
             }
         });
 
-        // Build the statistics string
         StringBuilder statistics = new StringBuilder();
         for (Map.Entry<String, Integer> entry : sortedEntries) {
             statistics.append(entry.getKey()).append(" - ").append(entry.getValue()).append("\n");
         }
 
-        // Trim the trailing whitespace
-        String result = statistics.toString().trim();
-
-        return result;
+        return statistics.toString().trim();
     }
 }
